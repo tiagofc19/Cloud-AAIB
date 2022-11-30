8514ee89e9968b5e3ef043fe18c2b705c87f871e
tiagofc19 <tiagofc2000@gmail.com>
2022-11-30 18:11:06 +0000

    2

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+#import context 
+import paho.mqtt.publish as publish
 
+
+publish.single("AAIB", "Lu gosta de gatinos", hostname="test.mosquitto.org")
 st.title('Gatinos')
+
+if st.button('Record'):
+    publish.single("AAIB-TL", "Lu gosta de gatinos", hostname="test.mosquitto.org")
