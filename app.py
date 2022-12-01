5eccab74616797916e4c5a91ec7a3a0d800d5ff1
tiagofc19 <tiagofc2000@gmail.com>
2022-12-01 18:57:28 +0000

    2

--- app.py
+++ app.py
@@ -2,7 +2,17 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import paho.mqtt.client as mqtt
+import json
+import time 
 
+st.title('Gatinos')
+st.header("Choose a wav or mp3 file")
+uploaded_file = st.file_uploader(" ")
+
+client = mqtt.Client("record_aaib")
+
+def on_subscribe(mqttc, obj, mid, granted_qos):
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
 
 def on_message(client, userdata, msg):
     topic=msg.topic
@@ -10,20 +20,29 @@ def on_message(client, userdata, msg):
     print("data Received type",type(m_decode))
     print("data Received",m_decode)
     print("Converting from Json to Object")
-    m_in=json.loads(m_decode) #decode json data
+    m_in=json.loads(m_decode) 
     print(type(m_in))
     print("broker 2 address = ",m_in["broker2"])
 
 
-st.title('Gatinos')
-y = st.header("Choose a wav or mp3 file")
-uploaded_file = st.file_uploader("")
+client.connect("test.mosquitto.org", 1883, 60)
+client.on_message = on_message
+client.on_subscribe = on_subscribe
+
+
+sub_ready = 0
+
+client.on_subscribe = on_subscribe
+client.subscribe("AAIB-LT", 0)
 
 if st.button('Record'):
-    client = mqtt.Client("record_aaib")
-    client.connect("test.mosquitto.org")
-    client. publish("AAIB-TL", payload="start")
+    print("0")
+    client.disconnect()
+    client.connect("test.mosquitto.org", 1883, 60)
+    client.publish("AAIB-TL", payload="start1")
 
-if st.button('recieve'):
-    client.on_message = on_message()
-    client.loop_forever()
+
+
+    
+
+
