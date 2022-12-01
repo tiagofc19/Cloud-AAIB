fbb8cf38df5b68825a41b9964e40e65a00f3cf6d
tiagofc19 <tiagofc2000@gmail.com>
2022-12-01 13:08:24 +0000

    4

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import context 
-import paho.mqtt.publish as publish
+import paho.mqtt.client as mqtt
 
 
 st.title('Gatinos')
+client.connect("test.mosquitto.org", port=8501, keepalive = 60)
+client. publish("AAIB-", payload="Start")
 
 if st.button('Record'):
-    publish.single("AAIB-TL", "Lu gosta de gatinos", hostname="test.mosquitto.org")
+    client = mqtt.Client("record_aaib")
+    client.connect("test.mosquitto.org", port=8501, keepalive = 60)
+    client. publish("AAIB-TL", payload="Start")
