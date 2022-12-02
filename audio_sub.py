810dbde1630982a9bcdfaf38ee9301ee0468399f
tiagofc19 <tiagofc2000@gmail.com>
2022-12-02 18:50:15 +0000

    3

--- audio_sub.py
+++ audio_sub.py
@@ -1,19 +1,40 @@
-#import context  # Ensures paho is in PYTHONPATH
 
 import paho.mqtt.client as mqtt
+import json
+from csv import writer
 
 
 def on_connect(mqttc, obj, flags, rc):
     print("rc: " + str(rc))
 
 
-def on_message(mqttc, obj, msg):
-    x = [x for x in msg.payload]
-    print(x)
-    #f = open('audio.wav', 'wb')
-    #f.write(msg.payload)
-    #f.close()
 
+def on_message(mqttc, obj, msg):
+    topic=msg.topic
+    #print(msg.payload.decode("utf-8","ignore"))
+    #y = msg.payload.decode("utf-8","ignore")
+    m_decode=str(msg.payload.decode("utf-8","ignore"))
+    #print("data Received type",type(m_decode))
+    #print("data Received",m_decode)
+    #print("Converting from Json to Object")
+    m_in=json.loads(m_decode) 
+    print(type(m_in))
+    #if y == 'new':
+    #    file = open('audios.csv', 'a')
+    #    file.close()
+
+    with open('audios.txt', 'a') as f:
+        for num in m_in:
+            f.write(str(num)+',')
+        f.write('\n')
+        f.close()
+
+    #with open('audios.csv', 'a') as f:
+    #    writer_object = writer(f)
+    #    writer_object.writerow(m_in)
+    #    f.close()
+    
+          
 
 def on_publish(mqttc, obj, mid):
     print("mid: " + str(mid))
@@ -27,18 +48,14 @@ def on_log(mqttc, obj, level, string):
     print(string)
 
 
-# If you want to use a specific client id, use
-# mqttc = mqtt.Client("client-id")
-# but note that the client id must be unique on the broker. Leaving the client
-# id parameter empty will generate a random id for you.
 mqttc = mqtt.Client()
 mqttc.on_message = on_message
 mqttc.on_connect = on_connect
 #mqttc.on_publish = on_publish
-#mqttc.on_subscribe = on_subscribe
+mqttc.on_subscribe = on_subscribe
 # Uncomment to enable debug messages
 # mqttc.on_log = on_log
-mqttc.connect("test.mosquitto.org", 1883, 60)
+mqttc.connect("mqtt.eclipseprojects.io", 1883, 60)
 mqttc.subscribe("AAIB-LT", 0)
 
 #rc = 0
